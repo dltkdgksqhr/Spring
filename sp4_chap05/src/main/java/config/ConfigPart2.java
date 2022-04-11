54a9351374e4e648da63fecd6a4d779b4fd259d7
dltkdgksqhr <78789ks@naver.com>
2022-04-11 11:47:47 +0900

    22/04/11 @Configuration 클래스를 주입받아 의존 설정하기

---
+++ sp4_chap05/src/main/java/config/ConfigPart2.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.MemberDao;
+import spring.MemberInfoPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
+
+@Configuration
+public class ConfigPart2 {
+	
+	@Autowired
+	private ConfigPart1 configPart1;
+	
+	@Bean
+	public MemberPrinter printer() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+		infoPrinter.setMemberDao(configPart1.memberDao);
+		infoPrinter.setPrinter(printer());
+		return infoPrinter;
+	}
+
+}
