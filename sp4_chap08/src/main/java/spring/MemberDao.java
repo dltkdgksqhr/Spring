f92b04c868c24ed4e9df25671660f8306c442b53
dltkdgksqhr <78789ks@naver.com>
2022-04-13 14:33:14 +0900

    22/04/13  4.6 Insert 쿼리 실행시, KeyHolder를 이용해서 자동 생성 키 값 구하기

--- sp4_chap08/src/main/java/spring/MemberDao.java
+++ sp4_chap08/src/main/java/spring/MemberDao.java
@@ -49,24 +49,27 @@ public class MemberDao {
 	}
 
 	public void insert(final Member member) {
-		KeyHolder keyHolder = new GeneratedKeyHolder();
+		KeyHolder keyHolder = new GeneratedKeyHolder(); //JdbcTamplate에서 자동으로 생성된 키 값 구하기 위해 사용
 		jdbcTemplate.update(new PreparedStatementCreator() {
 			@Override
 			public PreparedStatement createPreparedStatement(Connection con) 
 					throws SQLException {
+				// 파라미터로 전달받은 Connection을 이용해서 PreparedStatement 생성
 				PreparedStatement pstmt = con.prepareStatement(
 						"insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE) "+
 						"values (?, ?, ?, ?)",
-						new String[] {"ID"});
+						new String[] {"ID"}); // String[] id는 칼럼이 자동 증가 키 칼럼, 자동 생성되는 키 칼럼 목록을 지정할 때 사용된다.
+				// 인덱스 파라미터 값 설정
 				pstmt.setString(1,  member.getEmail());
 				pstmt.setString(2,  member.getPassword());
 				pstmt.setString(3,  member.getName());
 				pstmt.setTimestamp(4,  
 						new Timestamp(member.getRegisterDate().getTime()));
+				// 생성한 PreparedStatement 객체 리턴
 				return pstmt;
 			}
 		}, keyHolder);
-		Number keyValue = keyHolder.getKey();
+		Number keyValue = keyHolder.getKey(); //Number 타입으로 리턴 (KeyHolder)
 		member.setId(keyValue.longValue());
 	}
 
